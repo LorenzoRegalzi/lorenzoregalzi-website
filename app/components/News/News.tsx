60a95cc1d94e0b1bb9ebba34aad4acee10c25d03
Lorenzo Regalzi <lorenzo.regalzi@gmail.com>
2024-11-01 23:10:54 +0100

    add key on list and remove log

--- app/components/News/News.tsx
+++ app/components/News/News.tsx
@@ -6,14 +6,13 @@ export default async function News () {
     let data = await fetch('https://dev.to/api/articles?tag=programming')
     let posts = await data.json()
 
-    console.log(posts)
     return (
         <div>
             <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {posts.map((post : any) => (
           
 
-      <article className="flex max-w-xl flex-col items-start justify-between">
+      <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
         <div className="flex items-center gap-x-4 text-xs">
           <time  className="text-gray-500">{post.published_timestamp}</time>
           <a href={post.url} target='_blank' className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{post.type_of}</a>
